2275dd634f4bdd8257ffdb94234d8d63e5100a61
sousmahoro <soumalass590@gmail.com>
2022-07-01 13:41:48 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+#include <heure.h>
+
+/* plus d'en-têtes y vont */
+
+
+
+/* La doc de style betty pour la fonction main y va */
+
+int principal (vide)
+
+{
+
+		int n ;
+
+
+
+			srand(temps(0));
+
+				n = rand() - RAND_MAX / 2 ;
+
+					/* votre code y va */
+
+					retour (0);
+
+}
