e4cfb1e56a10b12de6d3073b8e2f2c88eabdc509
sousmahoro <soumalass590@gmail.com>
2022-07-28 09:54:25 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,41 +1,32 @@
 #include "main.h"
 #include <stdlib.h>
-/**
- * string_nconcat - function that concatenates two strings
- * wich contains s1 followed by the first n bytes of s2
- * @s1: string number 1
- * @s2: string number 2
- * @n: number of first bytes to concatenate
- * Return: returns the pointer to the allocated memory
- */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	char *pointer;
-	unsigned int i, j, s1_length;
+#include <stdio.h>
+#include <ctype.h>
 
-	if (s1 == NULL)
-		i = 0;
-	else
-		for (i = 0; s1[i]; i++)
-			;
-	if (s2 == NULL)
-		j = 0;
-	else
-		for (j = 0; s2[j]; j++)
-			;
-	if (n >= j)
-		n = j;
-	pointer = malloc(sizeof(char) * (i + n + 1));
-	if (pointer == NULL)
-		return (NULL);
-	s1_length = i;
-	for (i = 0; i < s1_length; i++)
-		pointer[i] = s1[i];
-	for (j = 0; j < n; j++)
+/**
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+unsigned long mul;
+int i, j;
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
 	{
-		pointer[i] = s2[j];
-		i++;
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
+
 	}
-	pointer[i] = '\0';
-	return (pointer);
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
 }
