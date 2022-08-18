a2356f152e92e7262ea99a02816e8a6e85039ee3
sousmahoro <soumalass590@gmail.com>
2022-08-18 04:05:48 -0700

    main.h

---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This is readme of 0x14. C - Bit manipulation
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#_MAIN_H_ ifndef
+#définir _MAIN_H_
+
+non signé int binary_to_uint(const char *b);
+void print_binary(non signé long int n);
+int _putchar(char c);
+int get_bit(long int n non signé, index int non signé);
+int set_bit(non signé long int *n, index int non signé);
+int clear_bit(non signé long int *n, index int non signé);
+non signé int flip_bits(non signé long int n, non signé long int m);
+int get_endianness(nul);
+
+#endif
