e191547054168f7a9b96a4043afdc153bd615930
sousmahoro <soumalass590@gmail.com>
2022-08-18 04:16:55 -0700

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,13 +1,13 @@
-#_MAIN_H_ ifndef
-#définir _MAIN_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-non signé int binary_to_uint(const char *b);
-void print_binary(non signé long int n);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 int _putchar(char c);
-int get_bit(long int n non signé, index int non signé);
-int set_bit(non signé long int *n, index int non signé);
-int clear_bit(non signé long int *n, index int non signé);
-non signé int flip_bits(non signé long int n, non signé long int m);
-int get_endianness(nul);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 #endif
